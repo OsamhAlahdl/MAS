c35c4b808999731ce236f2b79fa83f1a92e9acd2
OsamhAlahdl <samalahdl11@gmail.com>
2023-01-31 00:07:51 +0300

    create a.php
    new

---
+++ a.php
@@ -0,0 +1,122 @@
+<?php include("nav.php"); ?>
+<br><br><br><br>
+<div class="overflow-auto">
+<div class="container">
+<div class="container-title bg-light">
+  <h1 class="text-center ">بصيرة بيع / شراء</h1>
+</div>
+<br>
+<form class="row g-2 " style="direction: rtl;" >
+<div class="col-4">
+    <label for="form1Example1" class="form-label">رقم الوثيقة</label>
+    <input type="text" class="form-control " id="form1Example1">
+  </div>
+  <div class="col-4">
+    <label for="form1Example1" class="form-label">التاريخ</label>
+    <input type="date" class="form-control" id="form1Example1">
+  </div>
+  <div class="col-4">
+    <label for="form1Example1" class="form-label">نوع البصيره</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+  <div class="col-12  col-md-6">
+    <label for="form1Example1" class="form-label">اسم البائع</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+  <div class="col-12  col-md-6">
+    <label for="form1Example1" class="form-label">رقم البطاقة</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+  <div class="col-12 col-md-6">
+    <label for="form1Example1" class="form-label">اسم المشتري</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+  <div class="col-12 col-md-6">
+    <label for="form1Example1" class="form-label">رقم البطاقة</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+  
+  <div class="col-12 ">
+  <br>
+    <label for="form1Example1" class="form-label border-1" >الحدود</label>
+    <hr class="hr text-primary m-0">
+  </div>
+  
+
+  <div class="col-6">
+    <label for="form1Example1" class="form-label">من الشرق</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+
+  <div class="col-6">
+    <label for="form1Example1" class="form-label">من الغرب</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+
+  <div class="col-6">
+    <label for="form1Example1" class="form-label">من الحنوب</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+
+
+
+  <div class="col-6">
+    <label for="form1Example1" class="form-label">من الشمال</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+
+  <div class="col-12 ">
+  <br>
+    <label for="form1Example1" class="form-label ">المساحة</label>
+    <hr class="hr text-primary m-0">
+  </div>
+  
+  <div class="col-12 col-md-4">
+    <label for="form1Example1" class="form-label">مساحة الارضية</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+
+  <div class="col-12 col-md-4">
+    <label for="form1Example1" class="form-label">سعر المتر</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+
+  <div class="col-12 col-md-4">
+    <label for="form1Example1" class="form-label">اجمالي المبلغ</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+
+  
+  <div class="col-12 col-md-4">
+    <label for="form1Example1" class="form-label">الشاهد الأول</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+  
+  <div class="col-12 col-md-4">
+    <label for="form1Example1" class="form-label">الشاهد الثاني</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+  
+  <div class="col-12 col-md-4">
+    <label for="form1Example1" class="form-label">الشاهد الثالث</label>
+    <input type="text" class="form-control" id="form1Example1">
+  </div>
+
+  <div class="col-12 col-md-4">
+    <label for="inputState" class="form-label">المرفقات</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+
+  
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">ارسال</button>
+  </div>
+
+</div>
+...</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
